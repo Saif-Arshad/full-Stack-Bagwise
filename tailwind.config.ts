f3b186fc5192d8fd56c19dc04768391c2fa8ca0e
Saif Ur Rehman <saifurrehman.web@gmail.com>
2024-04-24 17:09:15 +0500

    tailwind configartion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
