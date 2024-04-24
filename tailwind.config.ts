2037bed7f2ee121de89a80907c5857f567871746
Saif Ur Rehman <saifurrehman.web@gmail.com>
2024-04-24 16:56:36 +0500

    Build error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,6 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  images: {
-    domains: ['fakestoreapi.com'],
-  },
   theme: {
     extend: {
       backgroundImage: {
